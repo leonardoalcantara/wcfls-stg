ecb4f23bc3f38ffe7ec4c8dd9631d692088f6836
Leonardo Alcantara <leonardo.alcantara@gmail.com>
2018-12-07 17:08:38 -0200

    Resolvido o bug do Internal Server error

--- app.py
+++ app.py
@@ -31,37 +31,37 @@ def index():
 			"Unable to get URL. Please make sure it's valid and try again."
 			)
 			return render_template('index.html', errors=errors, results=results)
-	if r:
-		# text processing
-		raw = BeautifulSoup(r.text, 'html.parser').get_text()
-		nltk.data.path.append('./nltk_data/') # set the path
-		tokens = nltk.word_tokenize(raw)
-		text = nltk.Text(tokens)
-		# remove punctuation, count raw words
-		nonPunct = re.compile('.*[A-Za-z].*')
-		raw_words = [w for w in text if nonPunct.match(w)]
-		raw_word_count = Counter(raw_words)
-		# stop words
-		no_stop_words = [w for w in raw_words if w.lower() not in stops]
-		no_stop_words_count = Counter(no_stop_words)
-		# save the results
-		results = sorted(
-			no_stop_words_count.items(),
-			key=operator.itemgetter(1),
-			reverse=True
-		)[:10]
-		try:
-			result = Result(
-				url=url,
-				result_all=raw_word_count,
-				result_no_stop_words=no_stop_words_count
-			)
-			db.session.add(result)
-			db.session.commit()
-		except	Exception as ex:
-			print(ex)
-			errors.append("Unable to add item to db")
+		if r:
+			# text processing
+			raw = BeautifulSoup(r.text, 'html.parser').get_text()
+			nltk.data.path.append('./nltk_data/') # set the path
+			tokens = nltk.word_tokenize(raw)
+			text = nltk.Text(tokens)
+			# remove punctuation, count raw words
+			nonPunct = re.compile('.*[A-Za-z].*')
+			raw_words = [w for w in text if nonPunct.match(w)]
+			raw_word_count = Counter(raw_words)
+			# stop words
+			no_stop_words = [w for w in raw_words if w.lower() not in stops]
+			no_stop_words_count = Counter(no_stop_words)
+			# save the results
+			results = sorted(
+				no_stop_words_count.items(),
+				key=operator.itemgetter(1),
+				reverse=True
+			)[:10]
+			try:
+				result = Result(
+					url=url,
+					result_all=raw_word_count,
+					result_no_stop_words=no_stop_words_count
+				)
+				db.session.add(result)
+				db.session.commit()
+			except	Exception as ex:
+				print(ex)
+				errors.append("Unable to add item to db")
 	return render_template('index.html', errors=errors, results=results)
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
