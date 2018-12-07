0b54d3de29c76c35b756d27ec3fed1c11663006c
Leonardo Alcantara <leonardo.alcantara@gmail.com>
2018-12-07 13:02:27 -0200

    Acrescenta a contagem de palavras

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
 import requests
+import operator
+import re
+import nltk
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from stop_words import stops 
+from collections import Counter
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 from models import Result
@@ -19,11 +26,40 @@ def index():
 		try:
 			url = request.form['url']
 			r = requests.get(url)
-			print(r.text)
 		except:
 			errors.append(
 			"Unable to get URL. Please make sure it's valid and try again."
 			)
+			return render_template('index.html', errors=errors, results=results)
+	if r:
+		# text processing
+		raw = BeautifulSoup(r.text, 'html.parser').get_text()
+		nltk.data.path.append('./nltk_data/') # set the path
+		tokens = nltk.word_tokenize(raw)
+		text = nltk.Text(tokens)
+		# remove punctuation, count raw words
+		nonPunct = re.compile('.*[A-Za-z].*')
+		raw_words = [w for w in text if nonPunct.match(w)]
+		raw_word_count = Counter(raw_words)
+		# stop words
+		no_stop_words = [w for w in raw_words if w.lower() not in stops]
+		no_stop_words_count = Counter(no_stop_words)
+		# save the results
+		results = sorted(
+			no_stop_words_count.items(),
+			key=operator.itemgetter(1),
+			reverse=True
+		)
+		try:
+			result = Result(
+				url=url,
+				result_all=raw_word_count,
+				result_no_stop_words=no_stop_words_count
+			)
+			db.session.add(result)
+			de.session.commit()
+		except:
+			errors.append("Unable to add item to db")
 	return render_template('index.html', errors=errors, results=results)
 
 if __name__ == '__main__':
